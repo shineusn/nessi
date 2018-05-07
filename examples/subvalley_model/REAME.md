2fd299868e8a867d9fc0b2b62f36193c10c1998c
PageotD <damien.pageot@gmail.com>
2018-05-07 16:48:30 +0200

    working on MPI inversion using PSO (part 2)

--- examples/subvalley_model/REAME.md
+++ examples/subvalley_model/REAME.md
@@ -1,7 +1,14 @@
 # Modeling and inversion example
 
 ## Observed data
+3 cores : 1 core per modeling
+
 `mpiexec -n 3 python subvalley_modeling_mpi.py`
 
 Output are SUfiles (vertical component only) in data folder
+
+## Inversion using PSO
+4 cores : 1 master and 1 slave per SUfile (modeling)
+
+`mpiexec -n 4 python subvalley_inversion_mpi.py`
 
--- examples/subvalley_model/subvalley_inversion_mpi.py
+++ examples/subvalley_model/subvalley_inversion_mpi.py
@@ -9,6 +9,7 @@
 # ------------------------------------------------------------------
 """
 Inversion of Rayleigh dispersion diagrams using PSO.
+
 :copyright:
     Damien Pageot (nessi.develop@protonmail.com)
 :license:
@@ -25,6 +26,7 @@ import matplotlib.pyplot as plt
 from mpi4py import MPI
 
 from nessi.io import SUdata
+from nessi.pso import Swarm
 
 
 # ------------------------------------------------------------------
@@ -50,25 +52,26 @@ if rank != 0:
 # >> Get information from SU header
 # ------------------------------------------------------------------
 
-# Get the source and receiver positions.
+# Get the source and receiver positions from SU headers.
 if rank != 0:
-    
+
     # Get scale for coordinates
     scalco = dobsz.header[0]['scalco']
     if scalco < 0:
         scale = -1./float(scalco)
     else:
         scale = float(scalco)
-    
+
     # Get the source position
     sx = dobsz.header[0]['sx']*scale
     sz = dobsz.header[0]['sy']*scale
-    
+
     # Get the receiver line
-    nrec = len(dobsz.header)    
+    nrec = len(dobsz.header)
     acq = np.zeros((nrec, 2), dtype=np.float32)
-    acq[:, 0] = dobsz.header[:]['gx']*scale
-    acq[:, 1] = dobsz.header[:]['gy']*scale
+    for ir in range(0, nrec):
+        acq[ir, 0] = dobsz.header[ir]['gx']*scale
+        acq[ir, 1] = dobsz.header[ir]['gy']*scale
 
 
 # >> Get time sampling
@@ -76,8 +79,8 @@ if rank != 0:
     ns = dobsz.header[0]['ns']
     dts = dobsz.header[0]['dt']/1000000.
     tmax = float(ns-1)*dts
-    
-    
+
+
 # ------------------------------------------------------------------
 # >> Define modeling parameters
 # ------------------------------------------------------------------
@@ -86,25 +89,78 @@ if rank != 0:
 
     # >> Run parameters
     runpar = {'name': 'subvalley',
-            'tmax': tmax, 'dt': 0.000005, # Time marching 
-            'snap': 0, 'dtsnap': 0.001}   # Snapshots
+              'tmax': tmax, 'dt': 0.000005, # Time marching
+              'snap': 0, 'dtsnap': 0.001}   # Snapshots
 
     # >> Model grid parameters
     modpar = {'n1': 51, 'n2': 301, 'dh': 0.5, # Grid dimensions
-            'ifsurf': 1, 'npml': 20, 'apml': 1200., 'ppml': 8} # Boundary conditions
+              'ifsurf': 1, 'npml': 20, 'apml': 1200., 'ppml': 8} # Boundary conditions
 
     # >> Acquisition parameters
     acqpar = {'sx': sx, 'sy': sz, # Source parameters
-            'f0': 15., 't0': 0.1, 'spread': -1, 'type': 2,
-            'acquisition': acq, 'dts': dts} # Receiver file name and data sampling
+              'f0': 15., 't0': 0.1, 'spread': -1, 'type': 2,
+              'acquisition': acq, 'dts': dts} # Receiver file name and data sampling
 
 
 # ------------------------------------------------------------------
 # >> Reference dispersion diagrams
 # ------------------------------------------------------------------
 
-# Calculating the dispersion diagrams
+# Calculating the reference dispersion diagrams using the MASW method
 if rank != 0:
     disp = dobsz.masw(vmin=200., vmax=1200., dv=5., fmin=10., fmax=50.)
-    plt.imshow(disp, aspect='auto', cmap='jet')
-    plt.show()
+
+# Block until all processes have reached this point.
+comm.Barrier()
+
+
+# ------------------------------------------------------------------
+# >> Initialize the swarm
+# ------------------------------------------------------------------
+
+# Initialize the number of particles on master and slaves.
+nindv = 49
+
+# Initialize the swarm on the master only.
+if rank == 0:
+    swarm = Swarm()
+    swarm.init_pspace('input/random_models.ascii')
+    swarm.init_particles(nindv)
+
+
+# ------------------------------------------------------------------
+# >> First evaluation
+# ------------------------------------------------------------------
+
+# Loop over particles
+for indv in range(0, nindv):
+
+    # Coarse to fine grid
+    if rank == 0:
+        print(rank, ' Coarse to fine grid.\n')
+
+    # Broadcast velocity and density models to slaves.
+
+    # Calculate observed data and evaluate
+    if rank != 0:
+        print(rank)
+        # Seismic modeling
+        # Rayleigh dispersion
+        # L2-norm
+
+    # MPI Barrier
+
+    # Gather L2 results on master and store the result
+
+# PSO update on master
+
+
+# ------------------------------------------------------------------
+# >> Process
+# ------------------------------------------------------------------
+
+# Loop over generations
+    # Coarse to fine  grid
+    # Observed data and evaluation
+    # L2-norm
+    # PSO update on master
