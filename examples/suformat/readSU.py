bc6b490f4b0697df29e1482790677b6f3198fe13
PageotD <damien.pageot@gmail.com>
2018-05-16 12:22:04 +0200

    update/add notebook

--- examples/suformat/readSU.py
+++ examples/suformat/readSU.py
@@ -8,13 +8,13 @@ from nessi.io import SUdata
 musc = SUdata()
 
 musc.read('musc_F50_01.su')
-#plt.subplot(121)
-#musc.image()
+plt.subplot(121)
+musc.image(clip=0.05, legend=1)
 
 muscw = copy.deepcopy(musc)
 #print(musc.header[0]['ns'].flags)
 muscw.wind(tmin=-0.0, tmax=0.3)
-#plt.subplot(122)
+plt.subplot(122)
 muscw.image(clip=0.05, legend=1)
 plt.show()
 
@@ -27,4 +27,3 @@ dt = 0.0001
 data = np.zeros((nr, ns), dtype=np.float32)
 test.create(data, dt)
 test.wind(tmin=0., tmax=0.1)
-print(muscw.header[:])
