afa4e942dc0d3da2361f3ec9958c1215ce157808
PageotD <nessi.develop@protonmail.com>
2018-05-21 09:47:43 +0200

    unittest PSO

--- nessi/pso/tests/test_init_pspace.py
+++ nessi/pso/tests/test_init_pspace.py
@@ -1,10 +1,10 @@
 import numpy as np
-from nessi.pso import init_pspace
+from nessi.pso import Swarm
 
 
 def test_init_pspace_one_line():
     """
-    pso_init_pspace test for one point search.
+    swarm.init_pspace test for one point search.
     """
     pspace_output = np.zeros((1, 2, 3), dtype=np.float32)
     pspace_output[0,0,0] = -3.0
@@ -13,12 +13,13 @@ def test_init_pspace_one_line():
     pspace_output[0,1,0] = -3.0
     pspace_output[0,1,1] = 3.0
     pspace_output[0,1,2] = 0.6
-    pspace_one_line = init_pspace('data/pspace_one_line.txt')
-    np.testing.assert_equal(pspace_one_line, pspace_output)
+    swarm = Swarm()
+    swarm.init_pspace('data/pspace_one_line.txt')
+    np.testing.assert_equal(swarm.pspace, pspace_output)
 
 def test_init_pspace_multi_line():
     """
-    pso_init_pspace test for multipoint search.
+    swarm.init_pspace test for multipoint search.
     """
     pspace_output = np.zeros((3, 2, 3), dtype=np.float32)
     pspace_output = np.array([
@@ -29,26 +30,26 @@ def test_init_pspace_multi_line():
         [[-3.0, 3.0, 0.6],
          [-3.0, 3.0, 0.6]]
         ], dtype=np.float32)
-    #pspace_output[0,0,0] = -3.0
-    #pspace_output[0,0,1] = 3.0
-    #pspace_output[0,0,2] = 0.6
-    #pspace_output[0,1,0] = -3.0
-    #pspace_output[0,1,1] = 3.0
-    #pspace_output[0,1,2] = 0.6
-    #pspace_output[1,0,0] = -3.0
-    #pspace_output[1,0,1] = 3.0
-    #pspace_output[1,0,2] = 0.6
-    #pspace_output[1,1,0] = -3.0
-    #pspace_output[1,1,1] = 3.0
-    #pspace_output[1,1,2] = 0.6
-    #pspace_output[2,0,0] = -3.0
-    #pspace_output[2,0,1] = 3.0
-    #pspace_output[2,0,2] = 0.6
-    #pspace_output[2,1,0] = -3.0
-    #pspace_output[2,1,1] = 3.0
-    #pspace_output[2,1,2] = 0.6
-    pspace_one_line = init_pspace('data/pspace_multi_line.txt')
-    np.testing.assert_equal(pspace_one_line, pspace_output)
+    swarm = Swarm()
+    swarm.init_pspace('data/pspace_multi_line.txt')
+    np.testing.assert_equal(swarm.pspace, pspace_output)
+
+def test_init_particles():
+    """
+    swarm.init_particles test.
+    """
+    output = np.array([[[-0.49786797,  1.321947  ],
+                        [-2.9993138 , -1.1860045 ],
+                        [-2.1194646 , -2.4459684 ]],
+                       [[-1.8824388 , -0.9266356 ],
+                        [-0.61939514,  0.23290041],
+                       [-0.4848329 ,  1.111317  ]]]
+                      , dtype=np.float32)
+    np.random.seed(1)
+    swarm = Swarm()
+    swarm.init_pspace('data/pspace_multi_line.txt')
+    swarm.init_particles(2)
+    np.testing.assert_equal(swarm.current, output)
     
 if __name__ == "__main__" :
     np.testing.run_module_suite()
